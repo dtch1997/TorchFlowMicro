f640d5b0d32a1449fe13f8cd52a1511cd3aa69c7
Daniel Tan <dtch1997@stanford.edu>
2020-08-03 12:36:32 +0800

    Fix syntax error in tuple shapes

--- tools/make_debug_fc_onnx_model.py
+++ tools/make_debug_fc_onnx_model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+INPUT_SHAPE = (300,)
 
 model = nn.Sequential(
     nn.Linear(300, 100, bias=True),
@@ -12,5 +13,5 @@ model = nn.Sequential(
 input_names = ['model_input']
 output_names = ['model_output']
 
-dummy_input = torch.randn(1, 300)
+dummy_input = torch.randn(1, *INPUT_SHAPE)
 torch.onnx.export(model, dummy_input, "debug_models/onnx/debug_fc.onnx", verbose=True, opset_version=11, input_names = input_names, output_names = output_names)
